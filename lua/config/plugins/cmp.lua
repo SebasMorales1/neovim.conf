53c5039987f7ff6d91888121b8c2b819bbc5cebb
Juan Sebastian Morales Cardona <sebasmorales05col@gmail.com>
2024-05-13 15:50:36 -0500

    Add cmp plugin

---
+++ lua/config/plugins/cmp.lua
@@ -0,0 +1,42 @@
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+
+local M = {}
+
+function M.setup()
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+        luasnip.lsp_expand(args.body)
+      end,
+    },
+    mapping = {
+      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-e>"] = cmp.mapping.abort(),
+      ["<C-n>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.choice_activate() then
+          luasnip.change_choice(1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      ["<C-p>"] = cmp.mapping.select_prev_item(),
+      ["<CR>"] = cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Insert,
+        select = true,
+      },
+      ["<C-space>"] = cmp.mapping.complete(),
+    },
+    sources = {
+      { name = "nvim_lsp" },
+      { name = "path" },
+      { name = "luasnip" },
+      { name = "buffer" },
+    },
+  })
+end
+
+return M
